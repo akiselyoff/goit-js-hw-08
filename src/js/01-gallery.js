90e0227e196ba810ea76dc90f3aa343cf253b0ab
kesel <a.kiselyoff@gmail.com>
2022-02-18 23:47:18 +0200

    hw-08-task01 change markup in html

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -13,12 +13,9 @@ const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
 function createGalleryItemsMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
-      return `<li>
-                  <a  class="gallery__item" href="${original}" style="display:block"
-    class="gallery__image">
+      return `<a  class="gallery__item" href="${original}" style="display:block" class="gallery__image">
                     <img class="gallery__image" src="${preview}" alt="${description}" />
-                  </a>
-              </li>`;
+                  </a>`;
     })
     .join('');
 }
